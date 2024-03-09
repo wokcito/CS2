8dc7dc4eefcabc57b552bdac05e940621ea86ae7
Maximiliano Kazanski <maxikazanski@gmail.com>
2024-03-09 17:38:55 -0300

    feat: add 'generateRequest' utility

---
+++ packages/dmarket/src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.dmarket.com'
+export const DMAR_ED25519 = 'dmar ed25519 '
---
+++ packages/dmarket/src/utils/types.ts
@@ -0,0 +1,10 @@
+/**
+ * Available methods for DMarket's API
+ * @see {@link https://docs.dmarket.com/v1/swagger.html}
+ */
+export enum AvailableMethods {
+	GET = 'GET',
+	POST = 'POST',
+	DELETE = 'DELETE',
+	PATCH = 'PATCH'
+}
